130f19f53c43690d35d8ca910b02d4c17aa6ef7f
lglof <lbglofch@uwaterloo.ca>
2020-04-27 16:14:50 -0400

    added flower generation

--- src/components/Workbench.vue
+++ src/components/Workbench.vue
@@ -3,11 +3,14 @@
     <Flower species="windflower" colour="white" />
     <Flower species="cosmos" colour="yellow" />
     <button v-on:click="newFlower()">New Flower!</button>
+    <div ref="container" />
   </div>
 </template>
 
 <script>
 import Flower from "./Flower";
+import Vue from "vue";
+var FlowerClass = Vue.extend(Flower);
 
 export default {
   name: "Workbench",
@@ -16,8 +19,11 @@ export default {
   },
   methods: {
     newFlower() {
-      console.log("NEW FLOWER");
-      // TODO: make a flower generator
+      var FlowerInstance = new FlowerClass({
+        propsData: { species: "cosmos", colour: "pink" }
+      });
+      FlowerInstance.$mount();
+      this.$refs.container.appendChild(FlowerInstance.$el);
     }
   }
 };
